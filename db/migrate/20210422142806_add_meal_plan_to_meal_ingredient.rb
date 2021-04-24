c59da776088e2776725d03941bdae1e75238e6c2
Flora Heathcote <flora@muud.design>
2021-04-24 22:54:16 +0100

    comment out first AddMealPlanToMealIngredient migration

--- db/migrate/20210422142806_add_meal_plan_to_meal_ingredient.rb
+++ db/migrate/20210422142806_add_meal_plan_to_meal_ingredient.rb
@@ -1,5 +1,5 @@
-class AddMealPlanToMealIngredient < ActiveRecord::Migration[6.0]
-  def change
-    add_column :meal_ingredients, :meal_plan, foreign_key: true
-  end
-end
+# class AddMealPlanToMealIngredient < ActiveRecord::Migration[6.0]
+#   def change
+#     add_column :meal_ingredients, :meal_plan, foreign_key: true
+#   end
+# end
