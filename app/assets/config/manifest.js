aba04c78ee44f45493a6a898cb782e8317a2b4d6
Aqeel <aqeelanwar307@gmail.com>
2023-11-16 13:58:27 +0500

    Fix assets issue

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,5 +2,8 @@
 
 //= link_tree ../images
 
+//= link administrate/application.css
+//= link administrate/application.js
+
 //= link_directory ../stylesheets .css
 //= link_directory ../../../node_modules/bootstrap-icons .svg
